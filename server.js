0d2f21fe2c07fbee8eb35c4565d1e1b030d4bcce
Dylan Avery <fossilfuelmusic@gmail.com>
2017-03-23 16:00:36 -0600

    add put endpoints

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/api/v1/users', function(req, res){
           res.status(200).json(users)
         })
       .catch(function(error) {
-            console.error('somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -37,7 +37,30 @@ app.post('/api/v1/users', function(req, res){
         })
         })
       .catch((error) => {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
+        })
+      .catch(function(error) {
+            res.status(422).send(error, '422')
+        })
+});
+
+app.put('/api/v1/users:id', function(req, res){
+  const first_name = req.body.first_name
+  const last_name = req.body.last_name
+  const email = req.body.email
+  const user = {first_name, last_name, email, updated_at: new Date}
+  database('users').where('id', request.body.id).update(user)
+        .then(()=>{
+          database('users').select()
+          .then((users) => {
+            res.status(200).json(users)
+        })
+        })
+      .catch((error) => {
+            res.status(404).send(error)
+        })
+      .catch(function(error) {
+            res.status(422).send(error, '422')
         })
 });
 
@@ -47,7 +70,7 @@ app.get('/api/v1/users/:id', function(req, res){
           res.status(200).json(users)
         })
       .catch(function(error) {
-            console.error('somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -60,7 +83,7 @@ app.delete('/api/v1/users/:id', function(req, res){
       })
       })
       .catch(function(error) {
-            console.error(error,'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -69,8 +92,8 @@ app.get('/api/v1/artists', function(req, res){
         .then((artists) => {
           res.status(200).json(artists)
         })
-      .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+        .catch(function(error) {
+          res.status(404).send(error)
         })
 });
 
@@ -81,7 +104,7 @@ app.get('/api/v1/artists/:id', function(req, res){
           res.status(200).json(artists)
         })
       .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -94,7 +117,7 @@ app.delete('/api/v1/artists/:id', function(req, res){
       })
       })
       .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -109,7 +132,28 @@ app.post('/api/v1/artists', function(req, res){
         })
         })
       .catch((error) => {
-            console.error(error, 'somethings wrong with db')
+          res.status(404).send(error)
+        })
+      .catch(function(error) {
+          res.status(422).send(error, '422')
+        })
+});
+
+app.put('/api/v1/artists:id', function(req, res){
+  const name = req.body.name
+  const artist = {name, updated_at: new Date}
+  database('artists').where('id', request.body.id).update(artist)
+        .then(()=>{
+          database('artists').select()
+          .then((artists) => {
+            res.status(200).json(artists)
+        })
+        })
+      .catch((error) => {
+          res.status(404).send(error)
+        })
+      .catch(function(error) {
+          res.status(422).send(error, '422')
         })
 });
 
@@ -119,7 +163,7 @@ app.get('/api/v1/songs', function(req, res){
           res.status(200).json(songs)
         })
       .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -129,7 +173,7 @@ app.get('/api/v1/songs/:id', function(req, res){
           res.status(200).json(songs)
         })
       .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -142,7 +186,7 @@ app.delete('/api/v1/songs/:id', function(req, res){
       })
       })
       .catch(function(error) {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
         })
 });
 
@@ -160,7 +204,31 @@ app.post('/api/v1/songs', function(req, res){
         })
         })
       .catch((error) => {
-            console.error(error, 'somethings wrong with db')
+            res.status(404).send(error)
+        })
+      .catch(function(error) {
+            res.status(422).send(error, '422')
+        })
+});
+
+app.put('/api/v1/songs:id', function(req, res){
+  const id = req.body.id
+  const name = req.body.name
+  const lyrics = req.body.lyrics
+  const artist_id = req.body.artist_id
+  const song = {id, name, lyrics, artist_id, updated_at: new Date }
+  database('songs').where('id', request.body.id).update(song)
+        .then(()=>{
+          database('songs').select()
+          .then((songs) => {
+            res.status(200).json(songs)
+        })
+        })
+      .catch((error) => {
+            res.status(404).send(error)
+        })
+      .catch(function(error) {
+            res.status(422).send(error, '422')
         })
 });
 
