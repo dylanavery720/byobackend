0509ffb6b54bc1cb8ba05f2b39651302d63dae57
Dylan Avery <fossilfuelmusic@gmail.com>
2017-03-26 10:50:19 -0600

    so much too much linting all done

--- nightmare.js
+++ nightmare.js
@@ -1,37 +1,38 @@
 const Nightmare = require('nightmare');
+
 const nightmare = Nightmare({ show: true });
 const nightmare2 = Nightmare({ show: true });
 
 
 const minify = (result) => {
   setTimeout(() => {
-      nightmare2.goto('http://www.willpeavy.com/minifier/')
+    nightmare2.goto('http://www.willpeavy.com/minifier/')
            .insert('#html', result)
            .wait(2000)
            .click('button')
            .wait(2000)
-           .evaluate(function () {
+           .evaluate(() => {
              return document.querySelector('#html').innerHTML;
            })
            .end()
-           .then(function (result) {
-             console.log(result)
+           .then((results) => {
+             console.log(results)
            })
-           .catch(function (error) {
+           .catch((error) => {
              console.error('Search failed:', error);
            });
-         }, 2000)
+  }, 2000)
 }
 
 nightmare
-  .goto(`https://genius.com/Bhagavad-gita-chapter-11-annotated`)
-  .evaluate(function () {
+  .goto('https://genius.com/Bhagavad-gita-chapter-11-annotated')
+  .evaluate(() => {
     return document.querySelector('div.lyrics').innerText;
   })
   .end()
-  .then(function (result) {
+  .then((result) => {
     minify(result)
   })
-  .catch(function (error) {
+  .catch((error) => {
     console.error('Search failed:', error);
   });
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser')
 
@@ -11,272 +12,271 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-  response.sendFile( __dirname + "/" + "index.html" )
+  response.sendFile(__dirname + '/' + 'index.html')
 })
 
-app.get('/api/v1/users', function(req, res){
+app.get('/api/v1/users', (req, res) => {
   database('users').select()
         .then((users) => {
           res.status(200).json(users)
         })
-      .catch(function(error) {
-            res.status(404).send(error)
-        })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.post('/api/v1/users', function(req, res){
+app.post('/api/v1/users', (req, res) => {
   const first_name = req.body.first_name
   const last_name = req.body.last_name
   const email = req.body.email
   const id = req.body.id || null
-  const user = {id, first_name, last_name, email, created_at: new Date}
+  const user = { id, first_name, last_name, email, created_at: new Date }
   database('users').insert(user)
-        .then(()=>{
+        .then(() => {
           database('users').select()
           .then((users) => {
             res.status(200).json(users)
-        })
+          })
         })
       .catch((error) => {
-            res.status(404).send(error)
-        })
-      .catch(function(error) {
-            res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
-app.put('/api/v1/users/:id', function(req, res){
+app.put('/api/v1/users/:id', (req, res) => {
   const first_name = req.body.first_name
   const last_name = req.body.last_name
   const email = req.body.email
-  const user = {first_name, last_name, email, updated_at: new Date}
+  const user = { first_name, last_name, email, updated_at: new Date }
   database('users').where('id', req.body.id).update(user)
-        .then(()=>{
+        .then(() => {
           database('users').select()
           .then((users) => {
             res.status(200).json(users)
-        })
+          })
         })
       .catch((error) => {
-            res.status(404).send(error)
-        })
-      .catch(function(error) {
-            res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
-app.get('/api/v1/users/:id', function(req, res){
+app.get('/api/v1/users/:id', (req, res) => {
   database('users').where('id', req.params.id).select()
         .then((users) => {
-          if(users.length<1) {
-            res.status(404).send({error: 'User does not exist, sorry its gotta be this wayyyyyyy'})
+          if (users.length < 1) {
+            res.status(404).send({ error: 'User does not exist, sorry its gotta be this wayyyyyyy' })
           } else {
-          res.status(200).json(users)
-        }
-      })
-      .catch(function(error) {
-            res.status(404).send(error)
+            res.status(200).json(users)
+          }
         })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.delete('/api/v1/users/:id', function(req, res){
+app.delete('/api/v1/users/:id', (req, res) => {
   database('users').where('id', req.params.id).del()
-      .then(()=>{
+      .then(() => {
         database('users').select()
         .then((users) => {
           res.status(200).json(users)
-      })
-      })
-      .catch(function(error) {
-            res.status(404).send(error)
         })
+      })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.get('/api/v1/artists', function(req, res){
-  let search = req.query.search
+app.get('/api/v1/artists', (req, res) => {
+  const search = req.query.search
   database('artists').select()
         .then((artists) => {
-          if(search){
-            let searched =  artists.filter((obj)=> {
-              if (obj.name == search) {
-              return obj
+          if (search) {
+            const searched = artists.filter((obj) => {
+              if (obj.name === search) {
+                return obj
               }
             })
-            if(searched.length<1){
-              res.status(404).send({error: 'no match'})
+            if (searched.length < 1) {
+              res.status(404).send({ error: 'no match' })
             } else {
               res.status(200).json(searched)
             }
           }
           else {
-              res.status(200).json(artists)
-            }
+            res.status(200).json(artists)
+          }
         })
-        .catch(function(error) {
+        .catch((error) => {
           res.status(404).send(error)
         })
 });
 
-app.get('/api/v1/artists/:id', function(req, res){
-
+app.get('/api/v1/artists/:id', (req, res) => {
   database('artists').where('id', req.params.id).select()
         .then((artists) => {
-          if(artists.length<1) {
-            res.status(404).send({error: 'Artist does not exist, sorry its gotta be this wayyyyyyy'})
+          if (artists.length < 1) {
+            res.status(404).send({ error: 'Artist does not exist, sorry its gotta be this wayyyyyyy' })
           } else {
-          res.status(200).json(artists)
-        }
-      })
-      .catch(function(error) {
-            res.status(404).send(error)
+            res.status(200).json(artists)
+          }
         })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.delete('/api/v1/artists/:id', function(req, res){
+app.delete('/api/v1/artists/:id', (req, res) => {
   database('artists').where('id', req.params.id).del()
-      .then(()=>{
+      .then(() => {
         database('artists').select()
         .then((artists) => {
           res.status(200).json(artists)
-      })
-      })
-      .catch(function(error) {
-            res.status(404).send(error)
         })
+      })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.post('/api/v1/artists', function(req, res){
+app.post('/api/v1/artists', (req, res) => {
   const name = req.body.name
   const id = req.body.id || null
-  const artist = {name, id, created_at: new Date}
+  const artist = { name, id, created_at: new Date }
   database('artists').insert(artist)
-        .then(()=>{
+        .then(() => {
           database('artists').select()
           .then((artists) => {
             res.status(200).json(artists)
-        })
+          })
         })
       .catch((error) => {
-          res.status(404).send(error)
-        })
-      .catch(function(error) {
-          res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
-app.put('/api/v1/artists/:id', function(req, res){
+app.put('/api/v1/artists/:id', (req, res) => {
   const name = req.body.name
-  const artist = {name, updated_at: new Date}
+  const artist = { name, updated_at: new Date }
   database('artists').where('id', req.body.id).update(artist)
-        .then(()=>{
+        .then(() => {
           database('artists').select()
           .then((artists) => {
             res.status(200).json(artists)
-        })
+          })
         })
       .catch((error) => {
-          res.status(404).send(error)
-        })
-      .catch(function(error) {
-          res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
-app.get('/api/v1/songs', function(req, res){
+app.get('/api/v1/songs', (req, res) => {
   database('songs').select()
         .then((songs) => {
           res.status(200).json(songs)
         })
-      .catch(function(error) {
-            res.status(404).send(error)
-        })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.get('/api/v1/songs/:id', function(req, res){
+app.get('/api/v1/songs/:id', (req, res) => {
   database('songs').where('id', req.params.id).select()
         .then((songs) => {
-          if(songs.length < 1) {
-            res.status(404).send({error: 'Song does not exist, sorry its gotta be this wayyyyyyy'})
+          if (songs.length < 1) {
+            res.status(404).send({ error: 'Song does not exist, sorry its gotta be this wayyyyyyy' })
           }
           res.status(200).json(songs)
         })
-      .catch(function(error) {
-            res.status(404).send(error)
-        })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.get('/api/v1/songs/:id/charcount', function(req, res){
+app.get('/api/v1/songs/:id/charcount', (req, res) => {
   database('songs').where('id', req.params.id).select()
         .then((songs) => {
-          if(songs.length < 1) {
-            res.status(404).send({error: 'Song does not exist, sorry its gotta be this wayyyyyyy'})
+          if (songs.length < 1) {
+            res.status(404).send({ error: 'Song does not exist, sorry its gotta be this wayyyyyyy' })
           }
-          res.status(200).json({charactercount:songs[0].lyrics.length})
+          res.status(200).json({ charactercount: songs[0].lyrics.length })
         })
-      .catch(function(error) {
-            res.status(404).send(error)
-        })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.delete('/api/v1/songs/:id', function(req, res){
+app.delete('/api/v1/songs/:id', (req, res) => {
   database('songs').where('id', req.params.id).del()
-      .then(()=>{
+      .then(() => {
         database('songs').select()
         .then((song) => {
           res.status(200).json(song)
-      })
-      })
-      .catch(function(error) {
-            res.status(404).send(error)
         })
+      })
+      .catch((error) => {
+        res.status(404).send(error)
+      })
 });
 
-app.post('/api/v1/songs', function(req, res){
+app.post('/api/v1/songs', (req, res) => {
   const id = req.body.id || null
   const name = req.body.name
   const lyrics = req.body.lyrics
   const artist_id = req.body.artist_id
-  const song = {id, name, lyrics, artist_id, created_at: new Date }
+  const song = { id, name, lyrics, artist_id, created_at: new Date }
   database('songs').insert(song)
-        .then(()=>{
+        .then(() => {
           database('songs').select()
-          .then((song) => {
-            res.status(200).json(song)
-        })
+          .then((songs) => {
+            res.status(200).json(songs)
+          })
         })
       .catch((error) => {
-            res.status(404).send(error)
-        })
-      .catch(function(error) {
-            res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
-app.put('/api/v1/songs/:id', function(req, res){
+app.put('/api/v1/songs/:id', (req, res) => {
   const id = req.body.id
   const name = req.body.name
   const lyrics = req.body.lyrics
   const artist_id = req.body.artist_id
-  const song = {id, name, lyrics, artist_id, updated_at: new Date }
+  const song = { id, name, lyrics, artist_id, updated_at: new Date }
   database('songs').where('id', req.body.id).update(song)
-        .then(()=>{
+        .then(() => {
           database('songs').select()
           .then((songs) => {
             res.status(200).json(songs)
-        })
+          })
         })
       .catch((error) => {
-            res.status(404).send(error)
-        })
-      .catch(function(error) {
-            res.status(422).send(error, '422')
-        })
+        res.status(404).send(error)
+      })
+      .catch((error) => {
+        res.status(422).send(error, '422')
+      })
 });
 
 
- if(!module.parent) {
-   app.listen(3001, () => {
-    console.log(`app listening port 3001`);
+if (!module.parent) {
+  app.listen(3001, () => {
+    console.log('app listening port 3001');
   });
 }
 
