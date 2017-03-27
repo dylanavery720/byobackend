6ab3bbbb9b7ccadd47b8c75f1dbae5c33f04d01b
Dylan Avery <fossilfuelmusic@gmail.com>
2017-03-27 04:48:14 -0600

    rid nodemon

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser')
 
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
-app.use(cors());
+// app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
